31874463404a62ba7877e56de1f786ed42cd07c6
xuanhung3081330@gmail.com <xuanhung3081330@gmail.com>
2024-05-29 13:06:02 +0000

    Debug

--- app.py
+++ app.py
@@ -20,13 +20,13 @@ def health_check():
 
 @app.route("/readiness_check")
 def readiness_check():
-    try:
-        count = db.session.query(Token).count()
-    except Exception as e:
-        app.logger.error(e)
-        return "failed", 500
-    else:
-        return "ok"
+    # try:
+    #     count = db.session.query(Token).count()
+    # except Exception as e:
+    #     app.logger.error(e)
+    #     return "failed", 500
+    # else:
+    return "ok"
 
 
 def get_daily_visits():
